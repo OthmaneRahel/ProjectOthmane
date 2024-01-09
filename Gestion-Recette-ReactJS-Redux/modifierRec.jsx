e1606e7ada7654739e4a75c72658070a9abf0e04
OTHMANE RAHEL <rahelothmane@gmail.com>
2024-01-09 18:47:25 +0100

    x

--- Gestion-Recette-ReactJS-Redux/modifierRec.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { modifier } from "./actions";
-import { useNavigate, useParams } from "react-router-dom";
-const modifierR = () =>{
-    const navigate = useNavigate();
-    const {id}=useParams()
-    
-}
