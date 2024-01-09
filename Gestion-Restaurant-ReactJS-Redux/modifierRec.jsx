f41970a9cf248e54ea9854cbe8dcc7b6378ba0cf
OTHMANE RAHEL <rahelothmane@gmail.com>
2024-01-09 18:10:45 +0100

    Gestion Restaurant

---
+++ Gestion-Restaurant-ReactJS-Redux/modifierRec.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { modifier } from "./actions";
+import { useNavigate, useParams } from "react-router-dom";
+const modifierR = () =>{
+    const navigate = useNavigate();
+    const {id}=useParams()
+    
+}
