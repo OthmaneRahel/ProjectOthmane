6eb12e39b347d34ec6c0718be381a2c9fd6c6436
OTHMANE RAHEL <rahelothmane@gmail.com>
2024-05-12 13:56:04 +0100

    hh

--- Gestion-Facebook-ReactJS-Redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import prodactPost from './reducers';
-
-const store = createStore(prodactPost);
-
-export default store;
--- Gestion-Repas-ReactJS-Redux/store.jsx
+++
@@ -1,4 +0,0 @@
-import { createStore } from "redux"
-import RepasReducer from "./reducer"
-const store = createStore(RepasReducer)
-export default store
